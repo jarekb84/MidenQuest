3443f15c704940bfac81be69cbf926c5b57d64db
Jerry Batorski <jerry.batorski@gmail.com>
2017-11-17 23:13:25 -0600

    Set default input position to top left

--- TileHighlighter.user.js
+++ TileHighlighter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MidenQuest - Tile Highlighter
 // @namespace    https://github.com/jarekb84/MidenQuest
-// @version      1.0.1
+// @version      1.0.2
 // @description  Highlights inputted list of map tiles
 // @updateURL    https://raw.githubusercontent.com/jarekb84/MidenQuest/master/TileHighlighter.user.js
 // @author       jarekb84
@@ -15,7 +15,7 @@
 
   function addContainer() {
     var container = $(
-      "<div id='tileHighlighter' style='position: fixed; top:0; right:0; background-color: white; padding: 5px' />"
+      "<div id='tileHighlighter' style='position: fixed; top:0; background-color: white; padding: 5px' />"
     );
     var textArea = $("<textarea id='tileHighlighter__tileData' rows='15' />");
 
